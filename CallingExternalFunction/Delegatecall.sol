e8da19bff3c8c3567ed3fcb54a1cd56f024b06dd
Hares27 <107697629+Hares27@users.noreply.github.com>
2023-08-26 13:37:41 +0530

    Update Delegatecall.sol

--- CallingExternalFunction/Delegatecall.sol
+++ CallingExternalFunction/Delegatecall.sol
@@ -1,1 +1,25 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+
+import "./TargetContract.sol";
+contract CallerContract{
+
+    uint public value;
+    address public sender;
+    string public name;
+    bool public callSuccess;
+
+
+    function testDelegate(string memory _name,TargetContract _target) public payable {
+        value=msg.value;
+        sender=msg.sender;
+        name=_name;
+        
+        (bool success,)=payable(address(_target)).delegatecall(abi.encodeWithSignature("targetFunction(string)",_name));
+
+         callSuccess=success;
+
+    }
+}
