fd9cedc3d69e4e0bf89539462c1e5a832d228ff3
Hares27 <107697629+Hares27@users.noreply.github.com>
2023-08-26 12:44:31 +0530

    Update CallerContract.sol

--- CallingExternalFunction/CallerContract.sol
+++ CallingExternalFunction/CallerContract.sol
@@ -1,1 +1,32 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract TargetContract{
+
+    function targetFunction(uint _number) public payable returns(uint)
+    {
+        return _number++;
+    }
+}
+
+
+contract CallerContract{
+
+    address private targetAddress;
+    bytes public result;
+
+   constructor(address _addr)
+   {
+       targetAddress=_addr;
+   }
+
+   function callExternal() public payable{
+       (bool success,bytes memory data)=targetAddress.call(abi.encodeWithSignature("targetFunction(uint256)",2));
+       require(success);
+       result=data;
+   }
+
+
+    
+}
