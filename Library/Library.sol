e01c70aa1ea8f33e0c93e6f9ae9b612c1051b5ae
Hares27 <107697629+Hares27@users.noreply.github.com>
2023-08-26 13:54:51 +0530

    Update Library.sol

--- Library/Library.sol
+++ Library/Library.sol
@@ -1,1 +1,29 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+library Array{
+    function remove(uint[] storage arr,uint index) internal {
+
+      require(arr.length >0," array length must be greater than zero");
+       arr[index]=arr[arr.length-1];
+       arr.pop();
+
+    }
+}
+contract TestArray{
+
+    using Array for uint[];
+
+    uint[] public arr;
+    
+    function addElementsToArray(uint _arrayElement) public{
+        arr.push(_arrayElement);
+
+    }
+     function testArrayRemove(uint _elementIndex) public{
+        arr.remove(_elementIndex);
+         
+     }
+
+}
