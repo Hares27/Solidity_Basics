3a6e6f2b4609b9eefe2049707f8a2c48cba69391
Hares27 <107697629+Hares27@users.noreply.github.com>
2023-08-26 13:40:25 +0530

    Update Targetcontract.sol

--- CallingExternalFunction/Targetcontract.sol
+++ CallingExternalFunction/Targetcontract.sol
@@ -1,1 +1,23 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+
+
+contract TargetContract{
+
+    uint public value;
+    address public sender;
+    string public name;
+   
+
+
+    function targetFunction(string memory _name) public payable {
+        value=msg.value;
+        sender=msg.sender;
+        name=_name;
+        
+       
+
+    }
+}
