0332f62cdd83c783b16a275b5666a9c434a1c0cf
Hares27 <107697629+Hares27@users.noreply.github.com>
2023-10-30 18:40:45 +0530

    Update SoulBoundToken.sol

--- SoulBoundToken/SoulBoundToken.sol
+++ SoulBoundToken/SoulBoundToken.sol
@@ -1,1 +1,33 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+
+contract SoulBoundToken{
+
+    struct Credentials{
+        string name;
+        string password;
+    }
+
+
+    mapping(address => bytes32) public CredentialHash;
+
+    function createCredentials(string memory _name,string memory _password) public{
+        require(CredentialHash[msg.sender] == bytes32(0),"The user has already created the credentials");
+      CredentialHash[msg.sender]=keccak256((abi.encode(_name,_password)));
+
+
+    }
+
+    function verifyCredentials(string memory _name,string memory _password)  view public returns(bool validation){
+        bytes32 verifyingHash=keccak256(abi.encode(_name,_password));
+        require(verifyingHash==CredentialHash[msg.sender],"The user credentials are not matching");
+        bool success=true;
+        return success;
+
+
+    
+        
+    }
+}
